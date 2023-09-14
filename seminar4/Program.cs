d4ad1b827a5b1508fa3b48883892f819daec5d62
matyashov1 <34284078+matyashov1@users.noreply.github.com>
2023-09-14 14:46:34 +0300

    Homework27

---
+++ seminar4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Promt(string messange)
+{
+    System.Console.Write(messange);
+    string read = System.Console.ReadLine();
+    int result = int.Parse(read);
+    return result;
+}
+
+int SumNumber( int a)
+{
+   int sumNumberresult = 0;
+   {
+    while (a > 0)
+    {
+        sumNumberresult = sumNumberresult + a % 10;
+        a = a/10;
+    }
+    return sumNumberresult;
+   }
+}
+
+int a = Promt("Введите число: ");
+Console.WriteLine(SumNumber(a));
